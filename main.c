2339d273075e50279f9e67d0cc5920ead956058b
artie37 <artieknapp@dyn-169-226-78-42.wireless.albany.edu>
2014-02-28 15:42:00 -0500

    02/28/2014: Final main function. Updated constant names. Added binaryStats function call. Add condition to not close outFile when binaryStats is called

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 // Arthur Knapp
 // AK146316
 
-// This is a program that will convert text from a formated
-// file into a binary file and vice versa. Each line of the
-// file has a string followed by a single tab character and
-// a non-negative integer.
+// This is a program that will convert text from a formated file into a binary
+// file and vice versa. Each line of the file has a string followed by a single tab
+// character and a non-negative integer. The program will also find the maximum and
+// minimum size string and, the maximum and minimum number then print them to the screen.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,8 +18,11 @@ int main(int argc, char * argv[])
     FILE *inFile;
     FILE *outFile;
     // Input and output file pointers
+    int outFileFlag = 0;
+    // Flag to close outfile. Set to 1 when commands '-t' and '-b'
+    // are used. Flag remains 0 when '-s' is used.
     
-    if (argc != THIRD && argc != FOUR)
+    if (argc != THIRD_INDEX && argc != FOUR_ARGS)
     // Check to see if the user entered the wrong number
     // of arguments. If they did, print an error message and
     // close program.
@@ -28,10 +31,10 @@ int main(int argc, char * argv[])
         exit(1);
     }
     
-    else if (strcmp(argv[FIRST], "-t") == 0 || strcmp(argv[FIRST], "-b") == 0)
+    else if (strcmp(argv[FIRST_INDEX], "-t") == 0 || strcmp(argv[FIRST_INDEX], "-b") == 0)
     // If the flag is either "-t" or "-b"
     {
-        if (argc != FOUR)
+        if (argc != FOUR_ARGS)
         // Check to see if the user entered the correct number
         // of arguments. If not, print an error message and
         // close program.
@@ -40,12 +43,15 @@ int main(int argc, char * argv[])
             exit(1);
         }
         
-        if (strcmp(argv[FIRST], "-t") == 0)
+        if (strcmp(argv[FIRST_INDEX], "-t") == 0)
         // If the flag is "-t".
         // This means that the input file is a text file and
         // it is going to be converted to a binary file.
         {
-            if ((inFile = fopen(argv[SECOND], "r")) == NULL)
+            outFileFlag = 1;
+            // Set outfile flag to 1 to close the outfile
+            
+            if ((inFile = fopen(argv[SECOND_INDEX], "r")) == NULL)
             // Check if input file opens. If not, print error
             // message and close program.
             {
@@ -53,7 +59,7 @@ int main(int argc, char * argv[])
                 exit(1);
             }
             
-            if ((outFile = fopen(argv[THIRD], "w")) == NULL)
+            if ((outFile = fopen(argv[THIRD_INDEX], "w")) == NULL)
             // Check if output file opens. If not, print error
             // message and close program.
             {
@@ -62,6 +68,7 @@ int main(int argc, char * argv[])
             }
             
             textToBinary(inFile, outFile);
+            // Call textToBinary function
         }
         
         else
@@ -69,7 +76,10 @@ int main(int argc, char * argv[])
         // This means that the input file is a binary file and
         // it is going to be converted to a text file.
         {
-            if ((inFile = fopen(argv[SECOND], "r")) == NULL)
+            outFileFlag = 1;
+            // Set outfile flag to 1 to close outfile.
+            
+            if ((inFile = fopen(argv[SECOND_INDEX], "r")) == NULL)
             // Check if input file opens. If not, print error
             // message and close program.
             {
@@ -77,7 +87,7 @@ int main(int argc, char * argv[])
                 exit(1);
             }
             
-            if ((outFile = fopen(argv[THIRD], "w")) == NULL)
+            if ((outFile = fopen(argv[THIRD_INDEX], "w")) == NULL)
             // Check if output file opens. If not, print error
             // message and close program.
             {
@@ -85,14 +95,14 @@ int main(int argc, char * argv[])
             }
             
             binaryToText(inFile, outFile);
+            // Call binaryToText function
         }
-        // proceed with program
     }
     
-    else if (strcmp(argv[FIRST], "-s") == 0)
+    else if (strcmp(argv[FIRST_INDEX], "-s") == 0)
     // If the argument is "-s"
     {
-        if (argc != THIRD)
+        if (argc != THIRD_INDEX)
         // Check to see if the user entered the correct number
         // of arguments. If not, print an error message and
         // close program.
@@ -101,14 +111,15 @@ int main(int argc, char * argv[])
             exit(1);
         }
         
-        if ((inFile = fopen(argv[SECOND], "r")) == NULL)
+        if ((inFile = fopen(argv[SECOND_INDEX], "r")) == NULL)
         // Check if input file opens. If not, print error
         // message and close program.
         {
             fprintf(stderr, "Error: Input File Did Not Open\n");
         }
         
-        // proceed with program
+        binaryStats(inFile);
+        // Call binaryStats function
     }
     
     else
@@ -119,6 +130,13 @@ int main(int argc, char * argv[])
     }
     
     fclose(inFile);
-    fclose(outFile);
+    
+    if (outFileFlag == 1)
+    // If outFileFlag is 1, close outfile. outFileFlag is only set
+    // to 1 when a command is entered that requires an outfile.
+    // Program seg faults if this is run when '-s' is called.
+    {
+       fclose(outFile);
+    }
 }
 
