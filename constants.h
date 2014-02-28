16b259fe5aac53a95b03aa5fdffd61a2f30de09a
artie37 <artieknapp@dyn-169-226-78-42.wireless.albany.edu>
2014-02-28 15:39:26 -0500

    02/28/2014: Final contsants header file. Added 'NULL_INDEX' and updated other names.

--- constants.h
+++ constants.h
@@ -5,12 +5,17 @@
 // This is a header file that defines all of the
 // constants used in this program.
 
-#define FIRST 1
+#define FIRST_INDEX 1
 // First spot of array of arguments
-#define SECOND 2
+#define SECOND_INDEX 2
 // Second spot of array of arguments
-#define THIRD 3
+#define THIRD_INDEX 3
 // Third spot of array of arguments
 // or used for 3 arguments
-#define FOUR 4
+#define FOUR_ARGS 4
 // To check if there are 4 arguments
+#define NULL_INDEX 255
+// The index value to insert the terminating character in the string
+#define STRING_SIZE 256
+// Size of character array. The array needs to hold 255 characters
+// so it is initialized to 256 to compensate for the terminating character.
